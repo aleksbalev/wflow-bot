d67e9e010a6ffa98af392534129e201e155f64f3
Aleks Balev <aleks.balev7899@gmail.com>
2023-11-01 17:54:19 +0100

    optimizing

--- src/slack.ts
+++ src/slack.ts
@@ -8,35 +8,37 @@ import { getBitbucketRepoChangelog } from "./util/bitbucket";
 async function handleSlashCommand(payload: SlackSlashCommandPayload) {
   switch (payload.command) {
     case "/release-log":
-      let regexp;
-
-      if (payload.text) {
-        regexp = new RegExp(`## ${payload.text}([\\s\\S]*?)---`);
-      } else {
-        regexp = new RegExp("## Unreleased([\\s\\S]*?)---");
-      }
-
+      const regexPattern = payload.text
+        ? `## ${payload.text}([\\s\\S]*?)---`
+        : "## Unreleased([\\s\\S]*?)---";
       const content = await getBitbucketRepoChangelog();
-      const transformedContent = `${content.data}`.match(regexp);
+      const transformedContent = `${content.data}`.match(
+        new RegExp(regexPattern),
+      );
 
       const client = await slackApi();
 
       try {
         if (transformedContent && transformedContent.length > 0) {
-          client.files.uploadV2({
+          await client.files.uploadV2({
             channel_id: payload.channel_id,
             initial_comment: "Here is your readme :wink:",
             content: transformedContent[0],
             filename: "CHANGELOG.md",
           });
         } else {
-          client.chat.postMessage({
+          await client.chat.postMessage({
             channel: payload.channel_id,
             text: "No matching content found in the repository. :disappointed:",
           });
         }
       } catch (err) {
         console.error(err);
+
+        return {
+          statusCode: 500,
+          body: "Internal Server Error",
+        };
       }
 
       break;
--- src/util/bitbucket.ts
+++ src/util/bitbucket.ts
@@ -3,7 +3,7 @@ import { Bitbucket } from "bitbucket";
 const repoOwner = "AleksBL";
 const repoSlug = "wbot-test";
 
-async function bitbucketApi() {
+function bitbucketApi() {
   return new Bitbucket({
     auth: {
       token: `${process.env.BITBUCKET_ACCESS_TOKEN}`,
@@ -12,14 +12,20 @@ async function bitbucketApi() {
 }
 
 export async function getBitbucketRepoChangelog() {
-  const bitbucket = await bitbucketApi();
+  const bitbucket = bitbucketApi();
 
-  const content = bitbucket.repositories.readSrc({
-    commit: "main",
-    repo_slug: repoSlug,
-    workspace: repoOwner,
-    path: "CHANGELOG.md",
-  });
+  try {
+    const content = await bitbucket.repositories.readSrc({
+      commit: "main",
+      repo_slug: repoSlug,
+      workspace: repoOwner,
+      path: "CHANGELOG.md",
+    });
 
-  return content;
+    return content;
+  } catch (err) {
+    console.error("Error fetching repository changelog:", err);
+
+    throw err;
+  }
 }
