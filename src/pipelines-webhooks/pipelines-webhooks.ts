5660ec1297ed884c78889baf55f16610321bbc44
Aleks Balev <aleks.balev7899@gmail.com>
2023-11-05 13:18:29 +0100

    debugging

--- src/pipelines-webhooks/pipelines-webhooks.ts
+++ src/pipelines-webhooks/pipelines-webhooks.ts
@@ -30,6 +30,7 @@ export const handler: Handler = async (event) => {
       const branch = branchSplitted[branchSplitted.length - 1];
       const headerName = relevantReposNamesMap.get(branch);
 
+      console.log(repoOwner, repoSlug);
       if (relevantRepos.includes(branch)) {
         const commits = await getPullRequestsCommits(
           resource.sourceVersion,
