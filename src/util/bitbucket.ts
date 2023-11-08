97fac3c0524a2c3141e84244f313fb0942ed2b1e
AleksB <aleksandr.balev@wflow.com>
2023-11-08 17:40:33 +0100

    fixing branch system

--- src/util/bitbucket.ts
+++ src/util/bitbucket.ts
@@ -5,17 +5,13 @@ const repoOwner =
 const repoSlug =
   process.env.NODE_MODE === "development" ? "wbot-test" : "wflow-main-app";
 
-function bitbucketApi() {
-  return new Bitbucket({
-    auth: {
-      token: `${process.env.BITBUCKET_ACCESS_TOKEN}`,
-    },
-  });
-}
+const bitbucket = new Bitbucket({
+  auth: {
+    token: `${process.env.BITBUCKET_ACCESS_TOKEN}`,
+  },
+});
 
 export async function getBitbucketRepoChangelog() {
-  const bitbucket = bitbucketApi();
-
   try {
     const content = await bitbucket.repositories.readSrc({
       commit: process.env.NODE_MODE === "development" ? "main" : "develop",
@@ -32,20 +28,22 @@ export async function getBitbucketRepoChangelog() {
   }
 }
 
+export async function getPullRequestsByCommit(commitHash: string) {
+  const pullRequests = await bitbucket.repositories.listPullrequestsForCommit({
+    commit: commitHash,
+    repo_slug: repoSlug,
+    workspace: repoOwner,
+  });
+
+  return pullRequests.data?.values;
+}
+
 export async function getPullRequestsCommits(
-  commit: string,
+  pullRequests: any[],
 ): Promise<{ data: CommitPayload } | null> {
-  const bitbucket = bitbucketApi();
-
   try {
-    const pullRequest = await bitbucket.repositories.listPullrequestsForCommit({
-      commit,
-      repo_slug: repoSlug,
-      workspace: repoOwner,
-    });
-
-    if (pullRequest.data.values && pullRequest.data.values.length > 0) {
-      const prId = pullRequest.data.values[0].id;
+    if (pullRequests && pullRequests.length > 0) {
+      const prId = pullRequests[0].id;
 
       if (prId) {
         return bitbucket.repositories.listPullRequestCommits({
@@ -65,7 +63,7 @@ export async function getPullRequestsCommits(
 export async function getVersionFromRepo(
   sourceBranch: string,
 ): Promise<string> {
-  const packageJSON = await bitbucketApi().repositories.readSrc({
+  const packageJSON = await bitbucket.repositories.readSrc({
     commit: sourceBranch,
     repo_slug: repoSlug,
     workspace: repoOwner,
